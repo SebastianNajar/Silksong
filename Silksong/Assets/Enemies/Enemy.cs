7e585f3a203330d670eaf2c0867c3cb4b7a5c755
SebastianNajar <pumasocks37@gmail.com>
2025-05-11 19:34:15 -0700

    Added New Enemy

--- Silksong/Assets/Enemies/Enemy.cs
+++ Silksong/Assets/Enemies/Enemy.cs
@@ -19,21 +19,27 @@ public class Enemy : MonoBehaviour
     //Physics
     private Rigidbody2D RB;
     public LayerMask ground;
+    private BoxCollider2D col;
+    private float raySize;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+
+        raySize = col.size.x / 1.7f;
+        Debug.Log(raySize);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Physics2D.Raycast(transform.position, -transform.right, 1.5f, ground) && facingLeft )
+        if (Physics2D.Raycast(transform.position, -transform.right, raySize, ground) && facingLeft )
         {
             TurnAround();
         }
-        else if (Physics2D.Raycast(transform.position, transform.right, 1.5f, ground) && !facingLeft)
+        else if (Physics2D.Raycast(transform.position, transform.right, raySize, ground) && !facingLeft)
         {
             TurnAround();
         }
